281ae63f3fa6b94224ad027f93c05274dcdb79e7
TEJENDER UPADHYAY <2020kucp1019@iiitkota.ac.in>
2021-08-04 19:18:49 +0530

    Update app.js
    Console.log removed

--- js/app.js
+++ js/app.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
         let tooltip = bar.children[0];
         tooltip.innerText = percentage + '%';
         bar.style.width = percentage + '%';
-        console.log(percentage);
     })
 
     $('.example').polygonizr({
