720724be1eccd4a3da3d97cc4e6eea12b32987d4
tarun_upadhyay <2020kucp1019@iiitkota.ac.in>
2021-08-04 13:59:22 +0530

    New sections added

--- js/app.js
+++ js/app.js
@@ -155,4 +155,73 @@ $(document).ready(function(){
         canvasZ: "auto"
         
       });
+
+
+
+    //   Image Filter Isotope Filter used as jQUery Library for gallery slider
+    var $wrapper = $('.portfolio__wrapper');
+
+    $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions : {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    let links =  document.querySelectorAll('.tabs a');
+
+    links.forEach(link => {
+
+        let selector = link.dataset.filter;
+        link.addEventListener('click', function(e)
+        {
+            e.preventDefault();
+
+
+            $wrapper.isotope({
+                filter : selector,
+                layoutMode : 'masonry',
+                animationOptions : {
+                    duration: 750,
+                    easing: 'linear'
+                }
+            });
+
+            links.forEach(link=>{
+                link.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+        });
+    })
+
+
+
+
+
+    // Magnify Popup jQuery library
+
+    $('.magnify').magnificPopup({
+        type: 'image',
+        gallery : {
+            enabled : true
+        },
+        zoom : {
+            enable: true
+        }
+    });
+
+
+    // Slider for text slide in my work section
+
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true
+
+    });
+
+
 });
